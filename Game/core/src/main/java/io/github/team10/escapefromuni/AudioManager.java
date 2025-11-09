2a2b65b0678d679f09848090480cb31f3dde616b
BenLeaver <benjackleaver@hotmail.com>
2025-11-09 09:01:15 +0000

    Update playEventSound() switch case to include default case

--- Game/core/src/main/java/io/github/team10/escapefromuni/AudioManager.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/AudioManager.java
@@ -101,6 +101,8 @@ public class AudioManager {
             case HIDDEN:
                 eventSound = hiddenEventSound;
                 break;
+            default:
+                break;
         }
         
         if (eventSound != null) {
